a61928292f01f4e0ab694df055f419ecf1ea9942
Gall-oDrone <piehavok@hotmail.com>
2024-07-24 19:27:04 -0600

    3-output-parsers-finish

--- ice_breaker.py
+++ ice_breaker.py
@@ -9,22 +9,32 @@ import os
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent 
 from third_parties.linkedin import scrape_linkedin_profile
-
+from output_parsers import summary_parser
 
 def ice_breaker_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username,
+        mock=True
+        )
 
     summary_template = """
-    given the information {information} about a cryptocurrency financial news article I want you to create:
+    given the information {information} about a person from linkedin {information}
+    I want you to create:
     1. A short summary
+    2. two interesting facts about them
+
+    \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()},
     )
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    #chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information": linkedin_data})
     print(res)
 
